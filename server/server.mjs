aee3d45d14e9ef56154b6271a312a611f90d896e
MD Shakil Khan <shakilkhan97135@gmail.com>
2026-08-21 00:34:42 +0200

    update

--- server/server.mjs
+++ server/server.mjs
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 10000;
 // CORS configuration
 app.use(cors({
   origin:
-  //  process.env.CLIENT_URL ||
+   process.env.CLIENT_URL ||
     'http://localhost:3000',
   credentials: true
 }));
